93e004ae40a81aa2796de7c7d68afa896e957c09
Alexander Dennenmoser <alexander.dennenmoser@outlook.de>
2023-03-12 11:26:26 +0100

    Prototype

---
+++ PhotoShare/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PhotoShare;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ PhotoShare/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PhotoShare;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
+
