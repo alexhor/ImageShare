41f02bd4170cc30a156e1a81ea2c4bc59765a897
Alexander Dennenmoser <alexander.dennenmoser@outlook.de>
2023-03-12 11:27:02 +0100

    Prototype

---
+++ PhotoShare/ImageFetcher.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace PhotoShare
+{
+	public class ImageFetcher
+	{
+		protected string sharingKey;
+
+        public ImageFetcher(string sharingKey)
+		{
+			this.sharingKey = sharingKey;
+
+        }
+
+		public void getPreview(string filename, string fileid)
+		{
+
+		}
+	}
+}
+
--- PhotoShare/MainPage.xaml.cs
+++ PhotoShare/MainPage.xaml.cs
@@ -1,25 +1,97 @@
-﻿namespace PhotoShare;
+﻿using System.Collections.ObjectModel;
+using System.Net;
+using System.Xml.Linq;
+using WebDav;
+
+namespace PhotoShare;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    protected static IWebDavClient client;
 
-	public MainPage()
+    protected static ImageFetcher imageFetcher;
+
+    protected static string sharingKey;
+
+
+    public MainPage()
+    {
+        sharingKey = "t2tqiAFNewekiTq";
+
+        WebDavClientParams clientParams = new WebDavClientParams
+        {
+            BaseAddress = new Uri("https://cloud.h-software.de/"),
+            Credentials = new NetworkCredential(sharingKey, "")
+        };
+        client = new WebDavClient(clientParams);
+
+
+
+        InitializeComponent();
+
+        // Load images
+        //Task task = getPhotoList();
+    }
+
+	private async Task getPhotoList()
 	{
-		InitializeComponent();
-	}
+        var propfindParams = new PropfindParameters
+        {
+            RequestType = PropfindRequestType.NamedProperties,
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+            Namespaces = new[]
+            {
+                new NamespaceAttr("oc", "http://owncloud.org/ns"),
+                new NamespaceAttr("nc", "http://nextcloud.org/ns"),
+                new NamespaceAttr("ocs", "http://open-collaboration-services.org/ns"),
+            },
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+            CustomProperties = new[]
+            {
+                XName.Get("fileid", "http://owncloud.org/ns"),
+                XName.Get("displayname", "DAV:")
+            }
+        };
+        
+        var result = await client.Propfind("https://cloud.h-software.de/public.php/webdav/", propfindParams);
+        if (result.IsSuccessful)
+		{
+            bool first = true;
+            // Show images
+            foreach (WebDavResource imageData in result.Resources)
+            {
+                // Show current folder on top
+                if (first) {
+                    first = false;
+                    this.CurrentFolder.Text = imageData.DisplayName;
+                    continue;
+                }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+                string fileid = "";
+                foreach (var property in imageData.Properties)
+                {
+
+                }
+                ClickableImage image = new ClickableImage(imageData.DisplayName, fileid, imageFetcher);
+                Application.Current.Dispatcher.Dispatch(() =>
+                {
+                        this.Gallery.Children.Add(image);
+                });
+                //ImageListWrapper.Append(image);
+            }
+
+            //ImageList.CollectionChanged += ImageList_CollectionChanged;
+        }
+        else
+		{
+            // handle an error
+		}
+    }
+
+    private void ImageList_CollectionChanged(object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
 }
 
 
